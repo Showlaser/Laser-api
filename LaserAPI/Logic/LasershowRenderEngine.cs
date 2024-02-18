2c0b3dbc43193059907bf38c9e02dfdd9796adf6
Vincent Darwinkel <vincent.darwinkel@hotmail.nl>
2024-02-18 22:24:47 +0100

    Refactor and added code

--- LaserAPI/Logic/LasershowRenderEngine.cs
+++ LaserAPI/Logic/LasershowRenderEngine.cs
@@ -1,6 +1,8 @@
 ﻿using LaserAPI.Models.Dto.Animations;
 using LaserAPI.Models.Dto.Lasershows;
 using LaserAPI.Models.Dto.Patterns;
+using LaserAPI.Models.Helper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,14 +10,17 @@ namespace LaserAPI.Logic
 {
     public static class LasershowRenderEngine
     {
-        public static IEnumerable<LasershowAnimationDto> GetLasershowAnimationsInTimelinePosition(LasershowDto lasershow, int timelinePosition) =>
-            lasershow.LasershowAnimations.Where(la => la.StartTimeMs >= timelinePosition);
+        public static IEnumerable<LasershowAnimationDto> GetLasershowAnimationsInTimelinePosition(LasershowDto lasershow, int timelinePosition)
+        {
+            return lasershow.LasershowAnimations
+                .Where(la => NumberIsBetweenOrEqual(timelinePosition, la.StartTimeMs, GetLasershowAnimationDuration(la) + la.StartTimeMs));
+        }
 
         public static IEnumerable<AnimationPatternDto> GetAnimationPatternsEqualOrAfterTimelinePosition(IEnumerable<LasershowAnimationDto> lasershowAnimationPatterns,
             int timelinePosition)
         {
             return lasershowAnimationPatterns.SelectMany(lsap => lsap.Animation.AnimationPatterns
-                                             .Where(ap => ap.StartTimeMs >= timelinePosition));
+                                             .Where(ap => NumberIsBetweenOrEqual(timelinePosition, ap.StartTimeMs, GetAnimationPatternDuration(ap) + ap.StartTimeMs)));
         }
 
         public static int GetAnimationPatternDuration(AnimationPatternDto animationPattern)
@@ -35,17 +40,70 @@ namespace LaserAPI.Logic
             return animationDurations.Max();
         }
 
-        public static IEnumerable<PointDto> GetPointsToDraw(LasershowDto lasershow, int timelinePosition)
+        private static List<AnimationPatternKeyFrameDto> GetKeyFramesBeforeTimelinePositionSortedByTimeDescending(PropertyEdited property, AnimationPatternDto animationPattern, int TimelinePositionMs)
+        {
+            List<AnimationPatternKeyFrameDto> aPKF = animationPattern.AnimationPatternKeyFrames
+                .FindAll(ak => ak.TimeMs < TimelinePositionMs && ak.PropertyEdited == property);
+
+            aPKF.Sort((a, b) => b.TimeMs - a.TimeMs);
+            return aPKF;
+        }
+
+        private static List<AnimationPatternKeyFrameDto> GetKeyFramesPastTimelinePositionSortedByTime(PropertyEdited property, AnimationPatternDto animationPattern, int TimelinePositionMs)
+        {
+            List<AnimationPatternKeyFrameDto> aPKF = animationPattern.AnimationPatternKeyFrames
+                .FindAll(ak => ak.TimeMs > TimelinePositionMs && ak.PropertyEdited == property);
+
+            aPKF.Sort((a, b) => a.TimeMs - b.TimeMs);
+            return aPKF;
+        }
+
+        public static PreviousCurrentAndNextKeyFramePerProperty GetPreviousCurrentAndNextKeyFramePerProperty(AnimationPatternDto animationPatternDto, int TimelinePositionMs)
+        {
+            PreviousCurrentAndNextKeyFramePerProperty previousNextAndCurrentKeyFramePerProperty = new()
+            {
+                Previous = [],
+                Current = animationPatternDto.AnimationPatternKeyFrames.FindAll(ak => ak.TimeMs == TimelinePositionMs),
+                Next = []
+            };
+
+            foreach (PropertyEdited propertyEdited in Enum.GetValues(typeof(PropertyEdited)))
+            {
+                AnimationPatternKeyFrameDto previous = GetKeyFramesBeforeTimelinePositionSortedByTimeDescending(
+                    propertyEdited, animationPatternDto, TimelinePositionMs).FirstOrDefault();
+                if (previous != null)
+                {
+                    previousNextAndCurrentKeyFramePerProperty.Previous.Add(previous);
+                }
+
+                AnimationPatternKeyFrameDto next = GetKeyFramesPastTimelinePositionSortedByTime(
+                    propertyEdited, animationPatternDto, TimelinePositionMs).FirstOrDefault();
+                if (next != null)
+                {
+                    previousNextAndCurrentKeyFramePerProperty.Next.Add(next);
+                }
+            }
+
+            return previousNextAndCurrentKeyFramePerProperty;
+        }
+
+        public static IEnumerable<PointDto> GetPointsToRender(LasershowDto lasershow, int timelinePosition)
         {
             IEnumerable<LasershowAnimationDto> lasershowAnimations = GetLasershowAnimationsInTimelinePosition(lasershow, timelinePosition);
-            IEnumerable<AnimationPatternDto> animationPatterns = GetAnimationPatternsEqualOrAfterTimelinePosition(lasershowAnimations, timelinePosition);
+            IEnumerable<AnimationPatternDto> animationPatternsToPlay = GetAnimationPatternsEqualOrAfterTimelinePosition(lasershowAnimations, timelinePosition);
 
-            foreach (AnimationPatternDto pattern in animationPatterns)
+            List<PointDto> pointsToRender = new();
+            foreach (AnimationPatternDto pattern in animationPatternsToPlay)
             {
-
+                //pattern.
             }
 
             return [];
+        }
+
+        public static bool NumberIsBetweenOrEqual(int number, int min, int max)
+        {
+            return number >= min && number <= max;
         }
     }
 }
--- LaserAPITests/Tests/Logic/LasershowRenderEngineTest.cs
+++ LaserAPITests/Tests/Logic/LasershowRenderEngineTest.cs
@@ -1,6 +1,7 @@
 ﻿using LaserAPI.Logic;
 using LaserAPI.Models.Dto.Animations;
 using LaserAPI.Models.Dto.Lasershows;
+using LaserAPITests.MockedModels.AnimationPatterns;
 using LaserAPITests.MockedModels.Lasershow;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
@@ -16,7 +17,7 @@ namespace LaserAPITests.Tests.Logic
             LasershowAnimationDto lasershowAnimation = new MockedLasershowAnimation().LasershowAnimation;
             AnimationPatternDto animationPattern = lasershowAnimation.Animation.AnimationPatterns.First();
             int durationInMs = LasershowRenderEngine.GetAnimationPatternDuration(animationPattern);
-            Assert.IsTrue(durationInMs > 0);
+            Assert.IsTrue(durationInMs == 50);
         }
 
         [TestMethod]
@@ -24,7 +25,7 @@ namespace LaserAPITests.Tests.Logic
         {
             LasershowAnimationDto lasershowAnimation = new MockedLasershowAnimation().LasershowAnimation;
             int durationInMs = LasershowRenderEngine.GetLasershowAnimationDuration(lasershowAnimation);
-            Assert.IsTrue(durationInMs > 0);
+            Assert.IsTrue(durationInMs == 50);
         }
 
         [TestMethod]
@@ -32,7 +33,7 @@ namespace LaserAPITests.Tests.Logic
         {
             LasershowAnimationDto lasershowAnimation = new MockedLasershowAnimation().LasershowAnimationList[1];
             int durationInMs = LasershowRenderEngine.GetLasershowAnimationDuration(lasershowAnimation);
-            Assert.IsTrue(durationInMs > 100);
+            Assert.IsTrue(durationInMs == 150);
         }
 
         [TestMethod]
@@ -40,13 +41,39 @@ namespace LaserAPITests.Tests.Logic
         {
             LasershowDto lasershow = new MockedLasershow().Lasershow;
             int durationInMs = LasershowRenderEngine.GetLasershowDuration(lasershow);
-            Assert.IsTrue(durationInMs > 100);
+            Assert.IsTrue(durationInMs == 250);
         }
 
         [TestMethod]
         public void GetPointsToRender()
         {
 
+        }
+
+        [TestMethod]
+        public void NumberIsBetweenOrEqual()
+        {
+            bool isBetweenOrEqual = LasershowRenderEngine.NumberIsBetweenOrEqual(100, 0, 150);
+            Assert.IsTrue(isBetweenOrEqual);
+        }
+
+        [TestMethod]
+        [DataRow(4, 0, 4, 25)]
+        [DataRow(0, 4, 4, 0)]
+        [DataRow(4, 0, 0, 1000)]
+        public void GetPreviousCurrentAndNextKeyFramePerProperty(
+            int previousCount,
+            int currentCount,
+            int nextCount,
+            int timelinePositionMs)
+        {
+            AnimationPatternDto animationPattern = new MockedAnimationPattern().AnimationPattern.First();
+            LaserAPI.Models.Helper.PreviousCurrentAndNextKeyFramePerProperty keyFrames = LasershowRenderEngine
+                .GetPreviousCurrentAndNextKeyFramePerProperty(animationPattern, timelinePositionMs);
+
+            Assert.IsTrue(keyFrames.Previous.Count == previousCount);
+            Assert.IsTrue(keyFrames.Current.Count == currentCount);
+            Assert.IsTrue(keyFrames.Next.Count == nextCount);
         }
     }
 }
